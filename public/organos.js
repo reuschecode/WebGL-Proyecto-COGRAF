b11202169de697970d08994f8166c04239e34d76
reuschecode <reusche_@hotmail.com>
2020-12-07 02:06:38 -0500

    final final final final final final FINAL

--- public/organos.js
+++ public/organos.js
@@ -1,19 +1,19 @@
 const organos = {
     "Organos":[
         {
-            "Id": "2traquea",
+            "Id": "2traquea001",
             "Nombre": "Traquea",
             "Info": "Vía respiratoria que va de la laringe hasta los bronquios (vías respiratorias grandes que llegan hasta los pulmones). También se llama caña del pulmón. Anatomía del sistema respiratorio.",
             "Img": "https://i.pinimg.com/474x/45/89/a2/4589a20b0f6d1a6c7e4e5bffa52073c1.jpg"
           },
           {
-            "Id": "2bronquio_1",
+            "Id": "2bronquio_1001",
             "Nombre": "Bronquio",
             "Info": "Conducto en que se bifurca la tráquea y que se va subdividiendo a su vez en ramificaciones cada vez más finas en los pulmones.",
             "Img": "https://slideplayer.es/slide/8679126/25/images/18/BRONQUIOS+Los+bronquios+son+conductos+de+fibrocart%C3%ADlago+m%C3%A1s+delgados+que+la+traquea.+Bronquio+principal+derecho+e+izquierdo..jpg"
           },
         {
-          "Id": "2bronquio_2",
+          "Id": "2bronquio_2001",
           "Nombre": "Bronquio",
           "Info": "Conducto en que se bifurca la tráquea y que se va subdividiendo a su vez en ramificaciones cada vez más finas en los pulmones.",
           "Img": "https://slideplayer.es/slide/8679126/25/images/18/BRONQUIOS+Los+bronquios+son+conductos+de+fibrocart%C3%ADlago+m%C3%A1s+delgados+que+la+traquea.+Bronquio+principal+derecho+e+izquierdo..jpg"
@@ -25,7 +25,19 @@ const organos = {
           "Img": "https://www.caracteristicas.co/wp-content/uploads/2017/02/cerebelo-e1563922105534.png"
         },
         {
+          "Id": "2cerebelo001",
+          "Nombre": "Cerebelo",
+          "Info": "Parte posterior del encéfalo, constituida por una masa de tejido nervioso y que se encarga de la coordinación muscular y otros movimientos no controlados por la voluntad.",
+          "Img": "https://www.caracteristicas.co/wp-content/uploads/2017/02/cerebelo-e1563922105534.png"
+        },
+        {
           "Id": "2cerebro_tronco",
+          "Nombre": "Cerebro tronco",
+          "Info": "El tronco del encéfalo conecta la médula espinal con los centros de pensamiento superior del encéfalo. Consta de tres estructuras: el bulbo raquídeo, la protuberancia y el cerebro medio. El bulbo raquídeo se continúa con la médula espinal y se conecta hacia arriba con la protuberancia.",
+          "Img": "https://asociacioneducar.com/sites/default/files/styles/node_main/public/field/image/tronco-encefalico_0.png?itok=cbwNWuSe"
+        },
+        {
+          "Id": "2cerebro_tronco001",
           "Nombre": "Cerebro tronco",
           "Info": "El tronco del encéfalo conecta la médula espinal con los centros de pensamiento superior del encéfalo. Consta de tres estructuras: el bulbo raquídeo, la protuberancia y el cerebro medio. El bulbo raquídeo se continúa con la médula espinal y se conecta hacia arriba con la protuberancia.",
           "Img": "https://asociacioneducar.com/sites/default/files/styles/node_main/public/field/image/tronco-encefalico_0.png?itok=cbwNWuSe"
@@ -37,7 +49,19 @@ const organos = {
           "Img": "https://cdnb.20m.es/yaestaellistoquetodolosabe/files/2015/03/Cu%C3%A1les-son-las-habilidades-que-se-desarrollan-en-cada-uno-de-los-hemisferios-que-nuestro-cerebro.jpg"
         },
         {
+          "Id": "2cerebro_i001",
+          "Nombre": "Cerebro (parte izquierda)",
+          "Info": "El hemisferio izquierdo es la parte motriz capaz de reconocer grupos de letras formando palabras, y grupos de palabras formando frases, tanto en lo que se refiere al habla, la escritura, la numeración, las matemáticas y la lógica, como a las facultades necesarias para transformar un conjunto de informaciones en palabras, gestos y pensamientos. ",
+          "Img": "https://cdnb.20m.es/yaestaellistoquetodolosabe/files/2015/03/Cu%C3%A1les-son-las-habilidades-que-se-desarrollan-en-cada-uno-de-los-hemisferios-que-nuestro-cerebro.jpg"
+        },
+        {
           "Id": "2cerebro_d",
+          "Nombre": "Cerebro (parte derecha)",
+          "Info": "El hemisferio derecho gobierna tantas funciones especializadas como el hemisferio izquierdo. Su forma de elaborar y procesar la información es distinta del hemisferio izquierdo. No utiliza los mecanismos convencionales para el análisis de los pensamientos que utiliza el hemisferio izquierdo. Es un hemisferio integrador, dentro de las facultades viso-espaciales no verbales, especializado en sensaciones, sentimientos, prosodia y habilidades especiales como las visuales y sonoras, por ejemplo las habilidades artísticas y musicales.",
+          "Img": "https://cdnb.20m.es/yaestaellistoquetodolosabe/files/2015/03/Cu%C3%A1les-son-las-habilidades-que-se-desarrollan-en-cada-uno-de-los-hemisferios-que-nuestro-cerebro.jpg"
+        },
+        {
+          "Id": "2cerebro_d001",
           "Nombre": "Cerebro (parte derecha)",
           "Info": "El hemisferio derecho gobierna tantas funciones especializadas como el hemisferio izquierdo. Su forma de elaborar y procesar la información es distinta del hemisferio izquierdo. No utiliza los mecanismos convencionales para el análisis de los pensamientos que utiliza el hemisferio izquierdo. Es un hemisferio integrador, dentro de las facultades viso-espaciales no verbales, especializado en sensaciones, sentimientos, prosodia y habilidades especiales como las visuales y sonoras, por ejemplo las habilidades artísticas y musicales.",
           "Img": "https://cdnb.20m.es/yaestaellistoquetodolosabe/files/2015/03/Cu%C3%A1les-son-las-habilidades-que-se-desarrollan-en-cada-uno-de-los-hemisferios-que-nuestro-cerebro.jpg"
@@ -47,6 +71,18 @@ const organos = {
           "Nombre": "Corazón",
           "Info": "El corazón es un órgano del tamaño aproximado de un puño. Está compuesto de tejido muscular y bombea sangre a todo el cuerpo. La sangre se transporta a todo el cuerpo a través de los vasos sanguíneos, unos tubos llamados arterias y venas. El proceso de transportar la sangre en todo el cuerpo se llama circulación.",
           "Img": "https://www.eje21.com.co/site/wp-content/uploads/2015/09/corazon-anatomia.jpg"
+        },
+        {
+          "Id": "2cora001",
+          "Nombre": "Corazón",
+          "Info": "El corazón es un órgano del tamaño aproximado de un puño. Está compuesto de tejido muscular y bombea sangre a todo el cuerpo. La sangre se transporta a todo el cuerpo a través de los vasos sanguíneos, unos tubos llamados arterias y venas. El proceso de transportar la sangre en todo el cuerpo se llama circulación.",
+          "Img": "https://www.eje21.com.co/site/wp-content/uploads/2015/09/corazon-anatomia.jpg"
+        },
+        {
+          "Id": "2estomago",
+          "Nombre": "Estómago",
+          "Info": "Órgano que forma parte del aparato digestivo. El estómago ayuda a digerir los alimentos al mezclarlos con jugos digestivos convirtiéndolos en líquido diluido.",
+          "Img": "https://nci-media.cancer.gov/pdq/media/images/792160.jpg"
         },
         {
             "Id": "2estomago_1",
@@ -66,6 +102,12 @@ const organos = {
             "Info": "Órgano que forma parte del aparato digestivo. El estómago ayuda a digerir los alimentos al mezclarlos con jugos digestivos convirtiéndolos en líquido diluido.",
             "Img": "https://nci-media.cancer.gov/pdq/media/images/792160.jpg"
           },
+          {
+            "Id": "2estamago001",
+            "Nombre": "Estómago",
+            "Info": "Órgano que forma parte del aparato digestivo. El estómago ayuda a digerir los alimentos al mezclarlos con jugos digestivos convirtiéndolos en líquido diluido.",
+            "Img": "https://nci-media.cancer.gov/pdq/media/images/792160.jpg"
+          },
         {
           "Id": "2higado",
           "Nombre": "Higado",
@@ -73,7 +115,7 @@ const organos = {
           "Img": "https://www.stanfordchildrens.org/content-public/topic/images/04/322504.jpeg"
         },
         {
-            "Id": "2higado_1",
+            "Id": "2higado001",
             "Nombre": "Higado",
             "Info": "El hígado regula la mayor parte de los niveles químicos de la sangre y excreta un producto llamado bilis, que ayuda a descomponer las grasas y las prepara para su posterior digestión y absorción. Toda la sangre que sale del estómago y de los intestinos atraviesa el hígado.",
             "Img": "https://www.stanfordchildrens.org/content-public/topic/images/04/322504.jpeg"
@@ -97,19 +139,31 @@ const organos = {
           "Img": "https://dacarett.com/wp-content/uploads/2015/01/Anatomia-del-Ojo-Humano.jpg"
         },
         {
+          "Id": "2ojo_i001",
+          "Nombre": "Ojo izquierdo",
+          "Info": "El ojo humano es un órgano fotorreceptor, cuya función, ya implícita, consiste en recibir los rayos luminosos procedentes de los objetos presentes en el mundo exterior y transformarlos en impulsos eléctricos que son conducidos al centro nervioso de la visión en la parte posterior del cerebro.",
+          "Img": "https://dacarett.com/wp-content/uploads/2015/01/Anatomia-del-Ojo-Humano.jpg"
+        },
+        {
           "Id": "2pankreas",
           "Nombre": "Páncreas",
           "Info": "El páncreas tiene dos funciones principales, la función exocrina y la función endocrina. Las células exocrinas del páncreas producen enzimas que ayudan a la digestión.",
           "Img": "https://upload.wikimedia.org/wikipedia/commons/7/7e/Blausen_0699_PancreasAnatomy2.png"
         },
         {
-          "Id": "2pulmon_d",
+          "Id": "2pankreas001",
+          "Nombre": "Páncreas",
+          "Info": "El páncreas tiene dos funciones principales, la función exocrina y la función endocrina. Las células exocrinas del páncreas producen enzimas que ayudan a la digestión.",
+          "Img": "https://upload.wikimedia.org/wikipedia/commons/7/7e/Blausen_0699_PancreasAnatomy2.png"
+        },
+        {
+          "Id": "2pulmon_d001",
           "Nombre": "Pulmón derecho",
           "Info": "Los pulmones son estructuras anatómicas pertenecientes al sistema respiratorio, se ubican en la caja torácica, a ambos lados del mediastino. Debido al espacio ocupado por el corazón, el pulmón derecho es más grande que su homólogo izquierdo.",
           "Img": "https://www.cdc.gov/spanish/cancer/lung/images/lungdiagram.jpg"
         },
         {
-          "Id": "2pulmon_i",
+          "Id": "2pulmon_i001",
           "Nombre": "Pulmón izquierdo",
           "Info": "Los pulmones son estructuras anatómicas pertenecientes al sistema respiratorio, se ubican en la caja torácica, a ambos lados del mediastino. Debido al espacio ocupado por el corazón, el pulmón derecho es más grande que su homólogo izquierdo.",
           "Img": "https://www.cdc.gov/spanish/cancer/lung/images/lungdiagram.jpg"
